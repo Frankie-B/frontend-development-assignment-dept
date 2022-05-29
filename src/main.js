43405b1ef54facffbed38885a0458f770b295b7e
Frankie Bukenya <frankie.bukenya@gmail.com>
2022-05-30 00:15:26 +0200

    +(feat): Add landing desktop

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// import './style.css';
+// import './styles/reset.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+  // Your code here
+});
