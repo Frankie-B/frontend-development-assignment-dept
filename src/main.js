492c664f0578f1f5b31d1c219c2acc63429405a4
Frankie Bukenya <frankie.bukenya@gmail.com>
2022-06-04 11:51:47 +0200

    chore(dependancy): Add gsap deps and import to main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-// import './style.css';
-// import './styles/reset.css';
+import { gsap } from 'gsap';
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM fully loaded and parsed');
-  // Your code here
-});
+const mainTimeline = () => {
+  const tl = gsap.timeline({});
+
+  console.log('Animation is loaded!');
+
+  tl.add('onLoad').fromTo('#app', { autoAlpha: 0 }, { duration: 0.25, autoAlpha: 1, ease: 'power2.out' });
+
+  return tl;
+};
+
+const init = () => {
+  mainTimeline();
+  console.log('DOM fully loaded and parsed', Animation);
+};
+
+window.onload = init();
